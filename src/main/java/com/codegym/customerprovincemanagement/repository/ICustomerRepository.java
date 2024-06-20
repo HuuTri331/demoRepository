7bc2857709d849d8afb0e3d61f4394d15480fdab
HuuTri331 <nguyenhuutri331@gmail.com>
2024-06-20 14:01:45 +0700

    update

---
+++ src/main/java/com/codegym/customerprovincemanagement/repository/ICustomerRepository.java
@@ -0,0 +1,12 @@
+package com.codegym.customerprovincemanagement.repository;
+
+
+import com.codegym.customerprovincemanagement.model.Customer;
+import com.codegym.customerprovincemanagement.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ICustomerRepository extends CrudRepository<Customer, Long> {
+    Iterable<Customer> findAllByProvince(Province province);
+}
---
+++ src/main/java/com/codegym/customerprovincemanagement/service/ICustomerService.java
@@ -0,0 +1,11 @@
+package com.codegym.customerprovincemanagement.service;
+
+import com.codegym.customerprovincemanagement.model.Customer;
+import com.codegym.customerprovincemanagement.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+public interface ICustomerService extends IGenerateService<Customer>{
+    Iterable<Customer> findAllByProvince(Province province);
+}
